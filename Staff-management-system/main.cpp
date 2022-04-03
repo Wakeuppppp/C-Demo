33a5ad24ba7546cc713ca98383bede11aa6edd3f
yuan <708966958@qq.com>
2022-04-03 23:25:21 +0800

    first commit

---
+++ Staff-management-system/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * -*- coding = utf-8 -*-
+ * Author: _谷安 
+ * @Time : 2022/4/3 23:16
+ * @Project_Name : C__demos 
+ * @File : main.cpp
+ * @Software :CLion
+ */
+
+#include "iostream"
+using namespace std;
+
+int mainStaff()
+{
+    cout << "hhh" << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * -*- coding = utf-8 -*-
+ * Author: _谷安 
+ * @Time : 2022/4/3 23:17
+ * @Project_Name : C__demos 
+ * @File : main.cpp
+ * @Software :CLion
+ */
+
+#include "Staff-management-system/main.cpp"
+
+
+int main() {
+    mainStaff();
+    return 0;
+}
